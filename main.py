c7e5d6e80d643fd2a0414b476e14b4f3659ee8d1
Erikcm99 <erikcm9999@gmail.com>
2026-05-24 00:53:07 +0200

    player visible and movable

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from logger import log_state 
+from player import Player
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
-    pygame.init()   
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -15,6 +18,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        dt = clock.tick(60) / 1000
+        player.update(dt)
         pygame.display.flip()
 
 if __name__ == "__main__":
